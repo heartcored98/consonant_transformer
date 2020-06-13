166b61bf099c8767fa5219a44b10024c02359f0e
heartcored98 <kevin.jo@dingbro.ai>
2020-06-13 08:30:06 +0000

    Complete confused midtail tokenizer

--- consonant/model/tokenization.py
+++ consonant/model/tokenization.py
@@ -4,20 +4,23 @@ import re
 import numpy as np
 # import torch
 
+from consonant.model.jamo import join_jamos
+
 
 class NGRAMTokenizer():
 
     BASE_CODE, HEAD, MID = 44032, 588, 28
+    UNKNOWN_HEAD = '@'
 
     # 초성 리스트. 00 ~ 18
     HEAD_LIST = ['ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅃ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ']
-    EXTRA_LIST = [' ', ',', '.', '?', '!', '~', '∼']
+    EXTRA_LIST = [' ', ',', '.', '?', '!', '~', UNKNOWN_HEAD]
 
     # 중성 리스트. 00 ~ 20
-    MID_LIST = ['ㅏ', 'ㅐ', 'ㅑ', 'ㅒ', 'ㅓ', 'ㅔ', 'ㅕ', 'ㅖ', 'ㅗ', 'ㅘ', 'ㅙ', 'ㅚ', 'ㅛ', 'ㅜ', 'ㅝ', 'ㅞ', 'ㅟ', 'ㅠ', 'ㅡ', 'ㅢ', 'ㅣ', '@']
+    MID_LIST = ['ㅏ', 'ㅐ', 'ㅑ', 'ㅒ', 'ㅓ', 'ㅔ', 'ㅕ', 'ㅖ', 'ㅗ', 'ㅘ', 'ㅙ', 'ㅚ', 'ㅛ', 'ㅜ', 'ㅝ', 'ㅞ', 'ㅟ', 'ㅠ', 'ㅡ', 'ㅢ', 'ㅣ']
 
     # 종성 리스트. 00 ~ 27 + 1(1개 없음)
-    TAIL_LIST = ['#', 'ㄱ', 'ㄲ', 'ㄳ', 'ㄴ', 'ㄵ', 'ㄶ', 'ㄷ', 'ㄹ', 'ㄺ', 'ㄻ', 'ㄼ', 'ㄽ', 'ㄾ', 'ㄿ', 'ㅀ', 'ㅁ', 'ㅂ', 'ㅄ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ', '@']
+    TAIL_LIST = ['', 'ㄱ', 'ㄲ', 'ㄳ', 'ㄴ', 'ㄵ', 'ㄶ', 'ㄷ', 'ㄹ', 'ㄺ', 'ㄻ', 'ㄼ', 'ㄽ', 'ㄾ', 'ㄿ', 'ㅀ', 'ㅁ', 'ㅂ', 'ㅄ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ']
 
     def __init__(self, ngram, head_list=None, mid_list=None, tail_list=None):
         self.ngram = ngram
@@ -25,9 +28,8 @@ class NGRAMTokenizer():
         self.mid_list = mid_list if mid_list else self.MID_LIST
         self.tail_list = tail_list if tail_list else self.TAIL_LIST
 
-        self.head2id = self.generate_head_ngram2id(self.EXTRA_LIST+self.head_list, self.ngram)
-        self.mid2id = {mid:i for i,mid in enumerate(self.mid_list)}
-        self.tail2id = {tail:i for i,tail in enumerate(self.tail_list)}
+        self.head2id, self.id2head = self.generate_head_ngram2id(self.EXTRA_LIST + self.head_list, self.ngram)
+        self.midtail2id, self.id2midtail = self.generate_midtail2id(self.mid_list, self.tail_list)
 
     def generate_head_ngram2id(self, head_list, ngram):
         """Generate all possible ngram combination of head consonant set.
@@ -40,10 +42,34 @@ class NGRAMTokenizer():
         Returns:
             dict: dictionary with the key of ngram tuple and index as value.
         """
-        ngram_list = ['[PAD]', '[CLS]', '[SEP]', '[UNK]'] + list(product(head_list, repeat = ngram))
+        ngram_list = ['[PAD]', '[CLS]', '[SEP]'] + list(product(head_list, repeat = ngram))
 
         ngram2id = {ngram_head: i for i, ngram_head in enumerate(ngram_list)}
-        return ngram2id
+        id2ngram = {i: ngram_head for i, ngram_head in enumerate(ngram_list)}
+        return ngram2id, id2ngram
+
+    def generate_midtail2id(self, mid_list, tail_list):
+        """Generate all possible mid-tail consonant combination.
+
+        Args:
+            mid_list ([type]): [description]
+            tail_list ([type]): [description]
+
+        Returns:
+            [type]: [description]
+        """
+        
+        midtail_list = [''] + list(product(mid_list, tail_list))
+        midtail2id = {midtail: i for i, midtail in enumerate(midtail_list)}
+        id2midtail = {i: midtail for i, midtail in enumerate(midtail_list)}
+        return midtail2id, id2midtail
+
+    def replace_unknown_head(self, heads):
+        heads = list(heads)
+        for i, char in enumerate(heads):
+            if char not in self.head_list + self.EXTRA_LIST:
+                heads[i] = self.UNKNOWN_HEAD
+        return ''.join(heads)
 
     def encode(self, sent_list, max_char_length=128, return_tensors=None, return_attention_mask=None):
         """Encode list of the sentence and return dictionary of head_token_ids, mid_token_ids, and tail_token_ids batch.
@@ -68,8 +94,7 @@ class NGRAMTokenizer():
             
         output = {
             'head_ids': stack(encoded_sent, 'head_ids', return_tensors),
-            'mid_ids': stack(encoded_sent, 'mid_ids', return_tensors),
-            'tail_ids': stack(encoded_sent, 'tail_ids', return_tensors)
+            'midtail_ids': stack(encoded_sent, 'midtail_ids', return_tensors),
         }
         if return_attention_mask:
             output.update(
@@ -89,8 +114,7 @@ class NGRAMTokenizer():
             list: [description]
         """
         heads = list()
-        mids = list()
-        tails = list()
+        midtails = list()
 
         for i, keyword in enumerate(sent[:max_char_length-2]): # truncate with max_char_length-2 due to [CLS] and [SEP] tokens
             # 한글 여부 check 후 분리
@@ -100,21 +124,17 @@ class NGRAMTokenizer():
                 heads.append(self.head_list[head])
 
                 mid = int((char_code - (self.HEAD * head)) / self.MID)
-                mids.append(self.mid_list[mid])
-
                 tail = int((char_code - (self.HEAD * head) - (self.MID * mid)))
-                tails.append(self.tail_list[tail])
+                midtails.append((self.mid_list[mid], self.tail_list[tail]))
+
             else: # For non-korean character, put character on initial consonant and put @ for mid/last consonant.
                 heads.append(keyword)
-                mids.append('@')
-                tails.append('@')
+                midtails.append('')
 
-        # Initialize token ids with zero vectors
+        # Initialize token ids with [PAD] tokens
         head_ids = np.zeros(max_char_length, dtype=np.int)
-        mid_ids = np.zeros(max_char_length, dtype=np.int)
-        mid_ids.fill(-100)
-        tail_ids = np.zeros(max_char_length, dtype=np.int)
-        tail_ids.fill(-100)
+        midtail_ids = np.zeros(max_char_length, dtype=np.int)
+        midtail_ids.fill(-100)
 
         # Calculate left, right offset
         if self.ngram % 2 == 0: # even ngram
@@ -124,11 +144,9 @@ class NGRAMTokenizer():
             left_offset = (self.ngram-1) // 2
             right_offset = (self.ngram-1) // 2
 
-        head_ids[0] = self.head2id['[CLS]']
-        mid_ids[0] = -100 # cls tokens should be ignored
-        tail_ids[0] = -100 # cls tokens should be ignored
+        head_ids[0] = self.head2id['[CLS]'] # Add CLS token at the begining of the sentence
         # Convert consonant to id
-        for i, (head, mid, tail) in enumerate(zip(heads, mids, tails)):
+        for i, (head, midtail) in enumerate(zip(heads, midtails)):
             ngram = heads[max(i-left_offset, 0):min(i+right_offset+1, len(heads))]
             if i < left_offset: # pad space on right side if needs
                 margin = left_offset - i
@@ -137,59 +155,72 @@ class NGRAMTokenizer():
                 margin = right_offset - (len(heads)-1-i)
                 ngram = ngram + [' '] * margin 
 
+            ngram = self.replace_unknown_head(ngram)
             ngram = tuple(ngram)
             head_ids[i+1] = self.head2id[ngram] if ngram in self.head2id else self.head2id['[UNK]'] # look up n-gram otherwise Unknown token
-            mid_ids[i+1] = self.mid2id[mid] 
-            tail_ids[i+1] = self.tail2id[tail]
+            midtail_ids[i+1] = self.midtail2id[midtail] 
         head_ids[i+2] = self.head2id['[SEP]'] # Add seperation token at the end of the sentence
 
         output = {
             'head_ids': head_ids,
-            'mid_ids': mid_ids,
-            'tail_ids': tail_ids
+            'midtail_ids': midtail_ids,
         }
 
         if return_attention_mask:
-            attention_mask = np.hstack([np.ones(len(heads), dtype=np.int), np.zeros(max_char_length - len(heads), dtype=np.int)])
+            attention_mask = np.hstack([np.ones(len(heads)+2, dtype=np.int), np.zeros(max_char_length - len(heads) - 2, dtype=np.int)])
             output.update(
                 {'attention_masks': attention_mask}
             )
         return output
             
 
-    def decode(self, head_ids, mid_ids, tail_ids):
+    def decode(self, head_ids, midtail_ids):
         """Decode the original sentence from the list of ids
         """
+        for (head_ids, midtail_ids) in zip(head_ids, midtail_ids):
+            for head, midtail in zip():
+                pass
+            print(head_ids, midtail_ids)
 
+    def decode_sent(self, head_ids, midtail_ids):
+        position = int(self.ngram / 2)
 
+        sent = ""
+        for head, midtail in zip(head_ids, midtail_ids):
+            if head in [0, 1, 2, 3]:
+                continue
+            sent += join_jamos(self.id2head[head][position] + ''.join(self.id2midtail[midtail]))
+
+        return sent
     
 
 if __name__ == '__main__':
-    sentence = ["내가 너 엄청 좋아해^", "너도 나 좋아하니?"]
-    tokenizer = NGRAMTokenizer(2)
+    sentence = ["각나?", "힣"]
+    tokenizer = NGRAMTokenizer(4)
 
     print("Num Head Vocab:", len(tokenizer.head2id))
-    print("Num  Mid Vocab:", len(tokenizer.mid2id))
-    print("Num Tail Vocab:", len(tokenizer.tail2id))
+    print("Num  Mid Vocab:", len(tokenizer.midtail2id))
 
     result = tokenizer.encode(sentence, max_char_length=30, return_attention_mask=True) #, return_tensors='pt')
     head_ids = result['head_ids']
-    mid_ids = result['mid_ids']
-    tail_ids = result['tail_ids']
+    midtail_ids = result['midtail_ids']
     attention_masks = result['attention_masks']
 
-    print(head_ids, mid_ids, tail_ids)
+    print(head_ids, midtail_ids)
     print("Head Consonant ID")
     print('->', head_ids[0])
 
     print()
-    print("Mid Consonant ID")
-    print('->', mid_ids[0])
-
-    print()
-    print("Tail Consonant ID")
-    print('->', tail_ids[0])
+    print("Midtail Consonant ID")
+    print('->', midtail_ids[0])
 
     print()
     print("Attention Mask")
     print('->', attention_masks[0])
+
+    print("=========================")
+
+    result = tokenizer.decode_sent(head_ids[0], midtail_ids[0])
+
+    print(tokenizer.replace_unknown_head('ㄱㄴ!^ㅎ'))
+    print(tokenizer.id2midtail[0])
